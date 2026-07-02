49e055a16641e304f5a17f0c0a5f2caba547963b
Hastune Miku <yap.kimsoon23@gmail.com>
2026-07-02 20:41:41 +0800

    add instruction suggest

--- src/main/java/tarc/assignment/adt/List.java
+++ src/main/java/tarc/assignment/adt/List.java
@@ -1,5 +1,13 @@
 package tarc.assignment.adt;
 
-public class List {
+public class List <T>{
     //Todo: who can who do
+//    public void add(T item)
+//    public void insert(int index, T item)
+//    public T remove(int index)
+//    public T get(int index)
+//    public T set(int index, T item)
+//    public int size()
+//    public boolean isEmpty()
+//    public void clear()
 }
--- src/main/java/tarc/assignment/adt/Queue.java
+++ src/main/java/tarc/assignment/adt/Queue.java
@@ -4,4 +4,11 @@ public class Queue {
 
     //TODO:
     // Standard booking
+//    public void enqueue(T item)
+//    public T dequeue()
+//    public T peek()
+//    public int size()
+//    public boolean isEmpty()
+//    public boolean isFull()
+//    public void clear()
 }
