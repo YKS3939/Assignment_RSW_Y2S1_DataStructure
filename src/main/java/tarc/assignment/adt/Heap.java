357f152db24f9b16cbb2fc195dfc6bdd6d20fad0
Hastune Miku <yap.kimsoon23@gmail.com>
2026-07-17 21:32:35 +0800

    deploy with ,heap,list

--- src/main/java/tarc/assignment/adt/Heap.java
+++ src/main/java/tarc/assignment/adt/Heap.java
@@ -1,13 +1,15 @@
 package tarc.assignment.adt;
 
-public class Heap {
+public class Heap <T>{
     //TODO: VIP Booking
-//    public void insert(T item)
-//    public T extractMax()
-//    public T peekMax()
-//    private void shiftUp(int index)
-//    private void shiftDown(int index)
-//    public int size()
-//    public boolean isEmpty()
-//    public void clear()
+    private T[] heap;
+    private int size;
+    //    public void insert(T item)
+    //    public T extractMax()
+    //    public T peekMax()
+    //    private void shiftUp(int index)
+    //    private void shiftDown(int index)
+    //    public int size()
+    //    public boolean isEmpty()
+    //    public void clear()
 }
